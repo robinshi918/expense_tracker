bcf2664b96b1fe6b211eb5ec80ac13ca264088aa
Robin Shi <robinshi918@hotmail.com>
2021-08-06 22:42:39 +1200

    remove databinding in fragment_transaction_detail.xml

--- app/src/main/java/org/robin/app/expensetracker/TransactionDetailFragment.kt
+++ app/src/main/java/org/robin/app/expensetracker/TransactionDetailFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -30,8 +29,12 @@ import java.util.*
 @AndroidEntryPoint
 class TransactionDetailFragment : Fragment() {
 
-    private val detailViewModel: TransactionDetailViewModel by viewModels()
+    private val viewModel: TransactionDetailViewModel by viewModels()
     private val safeArgs: TransactionDetailFragmentArgs by navArgs()
+
+    private lateinit var binding: FragmentTransactionDetailBinding
+
+    private var transaction: Transaction? = null
 
     @SuppressLint("SetTextI18n")
     override fun onCreateView(
@@ -40,25 +43,21 @@ class TransactionDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = DataBindingUtil.inflate<FragmentTransactionDetailBinding>(
-            inflater,
-            R.layout.fragment_transaction_detail,
-            container,
-            false
-        ).apply {
-            viewModel = detailViewModel
-            lifecycleOwner = viewLifecycleOwner
+        binding = FragmentTransactionDetailBinding.inflate(inflater, container, false)
 
-            viewModel?.transaction?.observe(lifecycleOwner!!){
-                Log.e("Robin", "transaction arrived!!!!1 \n $it")
-            }
+        viewModel.transaction.observe(viewLifecycleOwner){ t: Transaction ->
+            transaction = t
+        }
+        val transactionId = safeArgs.transactionId
 
-            categoryContainer.setOnClickListener {
-                findNavController().navigate(R.id.action_select_category)
-            }
+        binding.categoryContainer.setOnClickListener {
+            findNavController().navigate(R.id.action_select_category)
+        }
+
+        binding.apply {
 
             saveBtn.setOnClickListener {
-                detailViewModel.save()
+                viewModel.save()
                 findNavController().navigateUp()
             }
 
@@ -68,9 +67,13 @@ class TransactionDetailFragment : Fragment() {
                     .setMessage(getString(R.string.delete_transaction_dialog_body))
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .setPositiveButton(R.string.ok) { _, _ ->
-                        detailViewModel.delete()
+                        viewModel.delete()
                         findNavController().navigateUp()
-                        Toast.makeText(requireContext(), getString(R.string.transaction_deleted), Toast.LENGTH_LONG).show()
+                        Toast.makeText(
+                            requireContext(),
+                            getString(R.string.transaction_deleted),
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
                     .setNegativeButton(R.string.cancel, null).show()
             }
@@ -107,13 +110,11 @@ class TransactionDetailFragment : Fragment() {
         (requireActivity() as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setHasOptionsMenu(true)
 
-        val transactionId = safeArgs.transactionId
-
         return binding.root
     }
 
     /**
-     * handle BACK button click
+     * handle BACK button in Action Bar
      */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
